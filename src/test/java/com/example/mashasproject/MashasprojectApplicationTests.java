14280fee94d32f6adab954c261fadb524ef6f1a2
Мария <maria.kuchina.pochta@gmail.com>
2021-12-14 23:52:22 +0300

    Initial commit

---
+++ src/test/java/com/example/mashasproject/MashasprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mashasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashasprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
